a788e0a9eba226faaf3d5914f2a246d1de89b68c
Harry Chew <chewys96@gmail.com>
2020-06-22 10:03:39 +0800

    Update thumbnail_generator.py with comment

--- thumbnail_generator.py
+++ thumbnail_generator.py
@@ -42,12 +42,16 @@ def process_options():
 	return options
 
 def process_file(root, basename):
+	"""Generates a thumbnail of the given image."""
+	# Open the image that we need to process
 	filename = f'{root}/{basename}'
 	image = PIL.Image.open(filename)
 
+	# Generate the thumbnail
 	size = (128, 128)
 	image.thumbnail(size)
 
+	# Store the thumbnail in the thumbnails folder
 	new_name = f'thumbnails/{basename}'
 	image.save(new_name, "JPEG")
 	return new_name
