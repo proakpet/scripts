533cf1b31432a93bf1abb97f2ce515dba4c6731b
obaji <proakpet@gmail.com>
2022-01-26 11:19:43 +0100

    Modify four files in the repo
    renamed email.py to mail.py because it was interferring with a module of
    the same name in the send_reminders.py file.
    I added thumbnail_generator.py file to the repo

--- thumbnail_generator.py
+++ thumbnail_generator.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from concurrent import futures
 import argparse
 import logging
 import os
@@ -32,7 +31,7 @@ def process_options():
 		kwargs['level'] = logging.DEBUG
 	elif options.verbose:
 		kwargs['level'] = logging.INFO
-	elif options.quite:
+	elif options.quiet:
 		kwargs['level'] = logging.ERROR
 	else:
 		kwargs['level'] = logging.WARN
@@ -69,15 +68,12 @@ def main():
 	if not os.path.exist('thumbnails'):
 		os.mkdir('thumbnails')
 
-	#executor = futures.ProcessPoolExecutor()
-	executor = futures.ThreadPoolExecutor()
+
 	for root, _, files in os.walk('images'):
 		for basename in progress_bar(files):
 			if not basename.endswith('.jpg'):
 				continue
-			executor.submit(process_file, root,basename)
-	print('Waiting for all threads to finish.')
-	executor.shutdown()
+			process_file(root, basename)
 	return 0
 
 
